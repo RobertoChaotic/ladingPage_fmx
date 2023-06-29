34724dc29ead6bf1434e630bd1d75ddb7257e013
Roberto Proença <121270768+RobertoChaotic@users.noreply.github.com>
2023-06-29 20:38:33 -0300

    new

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function abrirmenu() {
   let BarraMenuAberto = document.getElementById("barra-menu");
 
   if (BarraMenuAberto.style.width == "0px") {
-    BarraMenuAberto.style.width = "90px";
+    BarraMenuAberto.style.width = "100px";
   } else {
     BarraMenuAberto.style.width = "0px";
   }
